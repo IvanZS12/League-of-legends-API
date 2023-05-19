94a8565d05095e262121746872361b50a49a5831
Iván Zárate Solís <132848111+IvanZS12@users.noreply.github.com>
2023-05-19 15:07:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,8 @@ def summoner_info(summoner_name, api_key):
         and saves it in a text file. For more information, please check the official 
         documentation at https://developer.riotgames.com/apis#summoner-v4. """
 
-    api_url = 'https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summoner_name
-    url = api_url + '?api_key=' + api_key
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Whale/3.20.182.14 Safari/537.36",
-        "Accept-Language": "es-419,es;q=0.9",
-        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Origin": "https://developer.riotgames.com",
-        "X-Riot-Token": "RGAPI-21440e4d-2243-4a07-a626-fc7e771e4a47"
-    }
-
-    response = requests.get(url, headers=headers)
+    url = 'https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summoner_name
+    response = requests.get(url, headers=headers) #Get headers on https://developer.riotgames.com
     res = response.json()
 
     if response.status_code == 200:
