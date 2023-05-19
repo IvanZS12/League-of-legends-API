434cdc0398e979de38b3ef988aea64b45a9e4a0c
Iván Zárate Solís <132848111+IvanZS12@users.noreply.github.com>
2023-05-19 15:05:57 -0600

    Update get-data-names.py

--- get-data-names.py
+++ get-data-names.py
@@ -9,15 +9,7 @@ while playersGathered < 2000:
 
     url = 'https://la1.api.riotgames.com/lol/league-exp/v4/entries/RANKED_SOLO_5x5/SILVER/IV?page='+ str(page)
     
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Whale/3.20.182.14 Safari/537.36",
-        "Accept-Language": "es-419,es;q=0.9",
-        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Origin": "https://developer.riotgames.com",
-        "X-Riot-Token": api_key
-    }
-
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers) #Get headers on https://developer.riotgames.com
 
     with open('summoners-name.txt', 'a', encoding='utf-8') as f:
         for summoner in response.json():
